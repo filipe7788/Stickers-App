9ab453e4170537bb7ac892f77c58d94dd30b310f
Filipe Cruz <filipe.cruz.ribeiro@gmail.com>
2026-04-26 22:12:13 -0300

    fix: add gesture handler jest setup and mock AppNavigator in App test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,6 +6,14 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('../src/navigation/AppNavigator', () => ({
+  AppNavigator: () => null,
+}));
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
     ReactTestRenderer.create(<App />);
